96c4b75103673189da13ea108e4778ebebef6a64
Santosh Suryawanshi <santosh.suryawanshi28@gmail.com>
2022-01-15 23:56:18 +0530

    Concurrency / waitGroup

---
+++ Curcurrency/waitGroup.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+var wg sync.WaitGroup
+
+func main() {
+	fmt.Println("CPU's:\t\t", runtime.NumCPU())
+	fmt.Println("Goroutines:\t", runtime.NumGoroutine())
+	fmt.Println("Go C calls:\t", runtime.NumCgoCall())
+	wg.Add(1)
+	go foo()
+	bar()
+	fmt.Println("Goroutines:\t", runtime.NumGoroutine())
+	fmt.Println("Go C calls:\t", runtime.NumCgoCall())
+	wg.Wait()
+}
+
+func foo() {
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+	}
+	wg.Done()
+}
+func bar() {
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+	}
+}
--- runtimeExample/runtimeExample.go
+++ runtimeExample/runtimeExample.go
@@ -6,7 +6,10 @@ import (
 )
 
 func main() {
-	fmt.Println(runtime.GOOS)
-	fmt.Println(runtime.GOARCH)
-	fmt.Println(runtime.Compiler)
+	fmt.Println("GO OS :\t\t", runtime.GOOS)
+	fmt.Println("GOARCH:\t\t", runtime.GOARCH)
+	fmt.Println("Compiler:\t", runtime.Compiler)
+	fmt.Println("CPU's:\t\t", runtime.NumCPU())
+	fmt.Println("Goroutines:\t", runtime.NumGoroutine())
+	fmt.Println("Go C calls:\t", runtime.NumCgoCall())
 }
