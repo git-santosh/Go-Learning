4a6481c9fdb1e0259ddbd5161bb08b654d7f5255
Santosh Suryawanshi <santosh.suryawanshi28@gmail.com>
2022-01-18 19:27:38 +0530

    Deferred/Panic/Recover and Error

--- Error/03/main.go
+++ Error/03/main.go
@@ -4,12 +4,17 @@ import (
 	"fmt"
 	"os"
 	"io/ioutil"
+	"log"
 	
 )
 func main(){
+	defer foo()
 	f,err:=os.Open("names.txt")
 	if err != nil { 
-		fmt.Println(err)
+		//fmt.Println(err)
+		//log.Println(err)
+		log.Panicln(err)
+		//log.Fatalln(err)
 		return
 	}
 	defer f.Close()
@@ -19,4 +24,8 @@ func main(){
 		return
 	}
 	fmt.Println(string(bs))
+}
+
+func foo(){ 
+	fmt.Println("when os.Exit() is called , the deffered function doesnt run")
 }
